6f5d72a14d7664ec1a1f030f652fb599ed3e09ae
cowglow <cowglow@gmail.com>
2019-05-20 18:51:52 +0200

    Functional programming component refactor

--- src/components/task-count/task-count.js
+++ src/components/task-count/task-count.js
@@ -3,21 +3,15 @@ import {Chip, Avatar} from "@material-ui/core";
 
 import styles from './task-count.module.css';
 
-class TaskCount extends React.Component {
+const TaskCount = ({count}) => (
+    <div className={styles.root} aria-label="List count">
+        {count ? (
+            <Chip avatar={<Avatar>{count}</Avatar>} color="primary" label="things to do."/>
+        ) : (
+            <Chip label={"Nothing to do"} color="primary" variant="outlined"/>
+        )}
+    </div>
+);
 
-    render() {
-        const {count} = this.props;
-
-        return (
-            <div className={styles.root} aria-label="List count">
-                {count ? (
-                    <Chip avatar={<Avatar>{count}</Avatar>} color="primary" label="things to do."/>
-                ) : (
-                    <Chip label={"Nothing to do"} color="primary" variant="outlined"/>
-                )}
-            </div>
-        )
-    }
-}
 
 export default TaskCount;
--- src/components/task-maker/task-maker.js
+++ src/components/task-maker/task-maker.js
@@ -1,85 +1,69 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from "prop-types";
 import {Input} from "@material-ui/core";
 
-class TaskMaker extends React.Component {
+// TODO: move to lib and test it.
+function getRandomPlaceholderText() {
+    const placeholderTextOptions = [
+        'What would you like to do next?',
+        'Type your task first, then manifest it!',
+        'Put another on your plate!',
+        'What\'s next to do?',
+        'Any other duties?!',
+        'You the boss, you know what to do!'
+    ];
 
-    state = {
-        placeholderText: ''
-    };
+    const optionCount = placeholderTextOptions.length;
+    const randomIndex = Math.floor(Math.random() * optionCount);
 
-    updatePlaceholderText() {
-        // TODO: make provider for this
-        const placeholderTextOptions = [
-            'What would you like to do next?',
-            'Type your task first, then manifest it!',
-            'Put another on your plate!',
-            'What\'s next to do?',
-            'Any other duties?!',
-            'You the boss, you know what to do!'
-        ];
-
-        const optionCount = placeholderTextOptions.length;
-        const randomIndex = Math.floor(Math.random() * optionCount);
-
-        this.setState({
-            placeholderText: placeholderTextOptions[randomIndex]
-        });
-    }
-
-    render() {
-        let {placeholderText} = this.state;
-
-        const {label} = this.props;
-
-        if (placeholderText === '') {
-            placeholderText = this.updatePlaceholderText()
-        }
-
-        const changeHandler = (event) => {
-            if (event.key === 'Enter') {
-                const bindedElement = event.target;
-                const elementValue = event.target.value;
-
-                const defaultValue = '';
-                const {callback} = this.props;
-
-                if (elementValue.length > 0) {
-                    // Prep data
-                    const callbackArgs = {
-                        label: elementValue,
-                        isChecked: false,
-                        key: new Date().getTime()
-                    };
-
-                    // Reset
-                    bindedElement.value = defaultValue;
-
-                    // Update the placeholder text
-                    this.updatePlaceholderText();
-
-                    // Execute Callback
-                    callback(callbackArgs);
-                }
-            }
-        };
-
-        return (
-            <Input
-                id="task-text"
-                type="text"
-                style={{size: '3em', boxSizing: 'border-box', padding: '1rem'}}
-                placeholder={placeholderText}
-                inputProps={{'arial-label': label}}
-                onKeyDown={changeHandler}
-                fullWidth={true}
-            />
-        );
-    }
+    return placeholderTextOptions[randomIndex];
 }
 
+const TaskMaker = ({callback, label}) => {
+
+    const [placeholderText, setPlaceholderText] = useState(getRandomPlaceholderText());
+
+    const changeHandler = (event) => {
+        if (event.key === 'Enter') {
+            const targetElement = event.target;
+
+            if (targetElement.value.length > 0) {
+                // Prep data
+                const callbackArgs = {
+                    key: new Date().getTime(),
+                    label: targetElement.value,
+                    isChecked: false
+                };
+
+                // Reset
+                targetElement.value = '';
+
+                // Update the placeholder text
+                setPlaceholderText(getRandomPlaceholderText());
+
+                // Execute Callback
+                callback(callbackArgs);
+            }
+        }
+    };
+
+    return (
+        <Input
+            id="task-text"
+            type="text"
+            style={{size: '3em', boxSizing: 'border-box', padding: '1rem'}}
+            placeholder={placeholderText}
+            inputProps={{'arial-label': label}}
+            onKeyDown={changeHandler}
+            fullWidth={true}
+        />
+    );
+
+};
+
 TaskMaker.propTypes = {
-    label: PropTypes.string.isRequired,
+    callback: PropTypes.func.isRequired,
+    label: PropTypes.string.isRequired
 };
 
 export default TaskMaker;
