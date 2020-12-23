7bb92630fc2e17306b00c3bc3b58f8421b2dc584
Philip Saa <cowglow@gmail.com>
2020-12-23 19:59:45 +0100

    Updates task maker

---
+++ src/components/task-maker/task-maker.story.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TaskMaker from "./task-maker";
+import {action} from "@storybook/addon-actions";
+
+export default {
+  name: "Components|TaskMaker",
+  component: TaskMaker,
+};
+
+export const TaskMakerStory = () => (
+    <TaskMaker createTask={action("create new task")}/>
+);
