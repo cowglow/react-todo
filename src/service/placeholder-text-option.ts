f11325a07b45c648b8652296d22b6c2c24db6d0e
Philip Saa <cowglow@gmail.com>
2020-12-23 19:58:57 +0100

    Adds random placeholder text option function with text

---
+++ src/service/placeholder-text-option.ts
@@ -0,0 +1,12 @@
+/** Used as the task maker placeholder text */
+
+const placeholderTextOptions = [
+  "What would you like to do next?",
+  "Type your task first, then manifest it!",
+  "Put another on your plate!",
+  "What's next to do?",
+  "Any other duties?!",
+  "You the boss, you know what to do!",
+];
+
+export default placeholderTextOptions;
